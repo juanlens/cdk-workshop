fcdd618315dafad76ac1bd897f59f7b428215985
Juan Lens <juanlens@gmail.com>
2022-04-26 17:49:44 +0200

    constructor

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,5 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -8,6 +9,13 @@ export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+      // defines an AWS Lambda resource
+      const hello = new lambda.Function(this, 'HelloHandler', {
+          runtime: lambda.Runtime.NODEJS_14_X, //execution environment
+          code: lambda.Code.fromAsset('lambda'), //code loaded from "lambda" directory
+          handler: 'hello.handler' // file is "hello", function is "handler"
+
+      });
 
 
 
